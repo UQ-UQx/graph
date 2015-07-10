6dde9707e57c33bdece23348d51596f059ea7d89
simultech <simultech@gmail.com>
2015-07-10 16:50:40 +1000

    Added database configuration

--- inc/header.php
+++ inc/header.php
@@ -4,6 +4,13 @@
 	require_once('config.php');
 	require_once('lib/lti.php');
 	$lti = new Lti($config,true);
+	if(isset($config['use_db']) && $config['use_db']) {
+		require_once('lib/db.php');
+		$db = new Db( 'mysql', $config['db']['hostname'], $config['db']['dbname'], $config['db']['username'], $config['db']['password'] );
+		//try {
+			
+		//} except
+	}
 ?>
 <head>
 	<link rel="stylesheet" type="text/css" href="www/css/bootstrap.min.css"></link>
